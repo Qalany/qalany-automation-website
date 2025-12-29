61f600640037fdf04d468875448c4d60e76c961e
Oshadha <oshadha.qalany@gmail.com>
2025-12-29 13:33:13 +0530

    minor update

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,7 +2,7 @@ import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
 import { ArrowLeft } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import qalanyLogo from "@/assets/qalany-logo.png";
+import qalanyLogo from "@/assets/qalany-logo-bright.png";
 
 const NotFound = () => {
   const location = useLocation();
